273b320dc46b0c9cd72919db25fe390f035df2ca
oyanagi1995 <lgh.serina.oyanagi@gmail.com>
2023-11-29 14:11:31 +0900

    231129 - ルートの設定（home）、viewの編集

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja" >
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="cache-control" content="no-cache, no-store">
+    <title>ホーム/タスク管理＜Panda＞</title>
+  </head>
+  <body>
+    <section>
+      <div class="main-manu">
+        <nav>
+          <ul>
+            <li>ホーム</li>
+            <li>マイタスク</li>
+          </ul>
+          <ul>
+            <li>プロジェクトボード１</li>
+            <li>プロジェクトボード２</li>
+          </ul>
+          <ul>
+            <li>設定</li>
+            <li>ログアウト</li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+    <section>
+      <div class="header">
+
+      </div>
+    </section>
+    <main>
+      <section>
+
+      </section>
+    </main>
+  </body>
+</html>
